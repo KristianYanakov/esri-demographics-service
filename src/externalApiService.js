978c5ac1d74ec013199965f9d2c8fef277e08bde
KristianYanakov <christianyanakov@gmail.com>
2026-01-15 23:24:37 +0200

    fix: always convert population to number + cleaned logs

--- src/externalApiService.js
+++ src/externalApiService.js
@@ -5,23 +5,13 @@ console.log('External API Service Loaded');
 export async function fetchData(){
     try{
         const response = await axios.get('https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/USA_Census_Counties/FeatureServer/0/query?where=1%3D1&outFields=population%2C+state_name&returnGeometry=false&f=pjson');
-        
-        // console.log(response.data.features[1].attributes.POPULATION);
-        // console.log(response.data.features[1].attributes.STATE_NAME);
-
-        console.log('Data fetched successfully');
-
         const stateDictionary = createStateDictionary(response.data);
         
-        console.log(stateDictionary);
-        console.log('State dictionary created successfully'+" LENGTH " + Object.keys(stateDictionary).length);
-
+        return stateDictionary;
     }catch (error){
         console.log(error);
     }
 }
-
-fetchData();
 
 function createStateDictionary(data){
     try{
@@ -30,7 +20,7 @@ function createStateDictionary(data){
         for (let i=0; i < data.features.length; i++){
             let feature = data.features[i];
             let stateName = feature.attributes.STATE_NAME;
-            let population = feature.attributes.POPULATION;
+            let population = Number(feature.attributes.POPULATION);
 
             if (stateName in stateDict){
                 stateDict[stateName] += population;
@@ -38,9 +28,9 @@ function createStateDictionary(data){
             }
             stateDict[stateName] = population;
         }
-        
+
         return stateDict;
-    
+
     }catch(error){
         console.log(error);
     }   
