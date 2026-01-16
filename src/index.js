e0a423fc7a5ad74549c3a855aa997cdc9cae49ff
KristianYanakov <christianyanakov@gmail.com>
2026-01-16 18:01:17 +0200

    feat: node-cron + writeData to json file and cron calls it

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 import express from 'express';
+import cron from 'node-cron';
+import fs from 'fs/promises';
+
+import { fetchData } from './externalApiService.js';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const FILE_NAME = 'stateData.json';
 
 app.use(express.json());
 app.use(express.static("public"));
+
+async function writeDataToFile(){
+    try{
+        const data = await fetchData();
+
+        await fs.writeFile(FILE_NAME, JSON.stringify(data, null, 2));
+        console.log('Data written to file successfully');
+    }catch(error){
+        console.error(error.message);
+    }
+}
+
+cron.schedule('0 * * * *', () => {
+    console.log('Cron job called:', new Date().toISOString());
+    writeDataToFile();
+});
+
+//First load on server start
+writeDataToFile();
 
 app.get('/', (req,res) => {
     let context = {"example": "my context value example!"};
     res.render('index.ejs', context);
 });
-
 
 //Server Port
 app.listen(PORT, () => {
